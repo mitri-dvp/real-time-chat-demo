08216cb1caaa0be4767960bdd19a4d1e9af11b5b
mitri-dvp <mitri.dvp@gmail.com>
2024-08-18 16:56:18 -0500

    Added shared folder

---
+++ shared/socketEvents.ts
@@ -0,0 +1,10 @@
+const socketEvents = {
+  DISCONNECT: "disconnect",
+  CONNECTION: "connection",
+  RECEIVE_MESSAGE: "receive-message",
+  SEND_MESSAGE: "send-message",
+  REGISTER: "register",
+  ONLINE_COUNT: "online-count",
+};
+
+export default socketEvents;
